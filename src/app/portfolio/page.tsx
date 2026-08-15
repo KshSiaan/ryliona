7cf6946bfaf5d48eb602f8e98fa50ee52e28940a
KshSiaan <kshsiaan@gmail.com>
2026-08-15 20:01:54 +0600

    quick

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import React, { Suspense } from "react";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className="sm:h-dvh relative! overflow-hidden grid grid-cols-2">
+      <div className="bg-primary"></div>
+      <div className=""></div>
+    </main>
+  );
+}
