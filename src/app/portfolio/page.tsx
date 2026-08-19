60106d29894e54a648138902ae885f9071605269
KshSiaan <kshsiaan@gmail.com>
2026-08-19 20:01:33 +0600

    quick

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,10 +4,10 @@ import { CloudShader } from "@/components/ui/cloud-shader";
 export default function Page() {
   return (
     <>
-      <header className="relative min-h-dvh w-dvw *:bg-gradient-to-b">
-        <CloudShader />
+      <header className="relative min-h-dvh *:bg-gradient-to-b">
+        <CloudShader className="h-dvh" />
       </header>
-      <main className="relative min-h-dvh w-dvw *:bg-gradient-to-b "></main>
+      <main className="relative min-h-dvh *:bg-gradient-to-b "></main>
     </>
   );
 }
